35440a1d4d63a3bfbadf6fe92921eac70de096d5
hbportfolio <henrikas.budrys@gmail.com>
2023-01-12 15:09:34 +0200

    fix-final

--- paskaitos/js01-student/js/custom.js
+++
@@ -1,8 +0,0 @@
-/*
-    .length
-    .toUpperCase() .toLowerCase()
-    .charAt .indexOf
-    .replace(",", " ")
-    .substring() .substr()
-    Math.round() Math.ceil() Math.floor() Math.max() Math.min() Math.random()*30
-*/
--- uzduotis6/js/custom.js
+++ uzduotis6/js/custom.js
@@ -6,3 +6,20 @@
     .substring() .substr()
     Math.round() Math.ceil() Math.floor() Math.max() Math.min() Math.random()*30
 */
+
+let num1 = 0;
+while(num1 <= 10) {
+    console.log(num1);
+    num1++;
+}
+
+let suma = 0;
+for(let t = 1; t <= 10; t++) {
+    suma += t;
+}
+
+suma = 0;
+skaic = [10, 20, 30, 40, 50, 60, 70, 80];
+for(let w = 0; w < skaic.length; w++) {
+    suma = suma + skaiciai[w];
+}
