452fb949ff642537dd106da85c7c16962ea66150
hbportfolio <henrikas.budrys@gmail.com>
2023-01-12 13:55:46 +0200

    7th commit?

---
+++ paskaitos/js01-student/js/custom.js
@@ -0,0 +1,8 @@
+/*
+    .length
+    .toUpperCase() .toLowerCase()
+    .charAt .indexOf
+    .replace(",", " ")
+    .substring() .substr()
+    Math.round() Math.ceil() Math.floor() Math.max() Math.min() Math.random()*30
+*/
